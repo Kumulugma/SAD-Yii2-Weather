4cbb473a5506a89047bcff46b9a4c077a040ab73
Michał Biel <micha@kumulugma.pl>
2022-04-28 14:33:34 +0200

    Połączenie z PostgreSQL

--- config/db.php
+++ config/db.php
@@ -2,13 +2,14 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'dsn' => 'pgsql:host=localhost;dbname=weather',
+    'username' => 'postgres',
+    'password' => '1234',
+    'schemaMap' => [
+        'pgsql' => 'tigrov\pgsql\Schema',
+    ],
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
 ];
