2aca865076008ccabbd69a8e84529ce751440af9
Michał Biel <micha@kumulugma.pl>
2022-05-01 19:23:24 +0200

    OrderBy w relacji

--- models/Locations.php
+++ models/Locations.php
@@ -1,34 +1,31 @@
 <?php
+
 namespace app\models;
 
 use yii\db\ActiveRecord;
 use app\models\SpeciesLocations;
 use app\models\Measurements;
 
-class Locations extends ActiveRecord
-{
-    
+class Locations extends ActiveRecord {
+
     /**
      * @return string the name of the table associated with this ActiveRecord class.
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return '{{locations}}';
     }
-    
-    public function getSpeciesLocations()
-    {
+
+    public function getSpeciesLocations() {
         return $this->hasMany(SpeciesLocations::class, ['location_id' => 'id']);
     }
-    
-    public function getMeasurements()
-    {
-        return $this->hasMany(Measurements::class, ['location_id' => 'id']);
+
+    public function getMeasurements() {
+        return $this->hasMany(Measurements::class, ['location_id' => 'id'])->
+                        orderBy(['measure_time' => SORT_ASC]);
     }
-    
-    
-    public static function getAllLocations()
-    {
+
+    public static function getAllLocations() {
         return self::find()->all();
     }
+
 }
